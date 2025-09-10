919a26ffa3c1fbf21c590a1e18485f5a4a4ba692
parson-harness <>
2025-09-10 22:57:37 +0000

    Project init

---
+++ test/infra/variables.tf
@@ -0,0 +1,12 @@
+variable "project_slug" {
+  type    = string
+  default = "test"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+variable "aws_region" {
+  type = string
+}
