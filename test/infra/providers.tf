a86bf256c9d61ad0502fb5a6d8872e3b75626348
Todd Parson <todd.parson@harness.io>
2025-09-10 21:55:22 -0500

    removing test repo

--- test/infra/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
-    random = { source = "hashicorp/random", version = "~> 3.6" }
-  }
-}
-provider "aws" {
-  region = var.aws_region # <-- picks up value from env/terraform.tfvars
-}
